43699992263b5be5b40daa394a472c58eaa3acfd
mikobhoie2299 <ryan_miguel_sarabia@dlsu.edu.ph>
2020-09-22 01:48:38 +0800

    Fixed indentions

--- ChatServer.java
+++ ChatServer.java
@@ -4,71 +4,72 @@ import java.net.*;
 import static java.lang.System.out;
 
 public class ChatServer {
-  Vector<String> users = new Vector<String>();
-  Vector<HandleClient> clients = new Vector<HandleClient>();
+	Vector<String> users = new Vector<String>();
+	Vector<HandleClient> clients = new Vector<HandleClient>();
 
-  public void process() throws Exception  {
-      ServerSocket server = new ServerSocket(9999,10, InetAddress.getLocalHost());
-	  out.println("Server Started...");
-	  out.println("Server listening on port: " + 	server.getLocalPort());
-	  out.println("IP address: " + server.getInetAddress().getHostAddress());
-	  int i =0;
-      while( true) {
- 		 	Socket client = server.accept();
- 		 	HandleClient c = new HandleClient(client);
-			clients.add(c);
-	 }
-  }
-  public static void main(String ... args) throws Exception {
-      new ChatServer().process();
-  } // end of main
+	public void process() throws Exception  {
+		ServerSocket server = new ServerSocket(9999,10, InetAddress.getLocalHost());
+		out.println("Server Started...");
+		out.println("Server listening on port: " + 	server.getLocalPort());
+		out.println("IP address: " + server.getInetAddress().getHostAddress());
+		int i =0;
+		while( true) {
+				Socket client = server.accept();
+				HandleClient c = new HandleClient(client);
+				clients.add(c);
+		}
+	}
+	public static void main(String ... args) throws Exception {
+		new ChatServer().process();
+	} // end of main
 
-  public void broadcast(String user, String message)  {
+  	public void broadcast(String user, String message)  {
 	    // send message to all connected users
 	    for ( HandleClient c : clients )
-	       if ( ! c.getUserName().equals(user) )
-	          c.sendMessage(user,message);
-  }
+	    	if ( ! c.getUserName().equals(user))
+	        	c.sendMessage(user,message);
+ 	}
 
-  class  HandleClient extends Thread {
+  	class  HandleClient extends Thread {
         String name = "";
-	BufferedReader input;
-	PrintWriter output;
+		BufferedReader input;
+		PrintWriter output;
 
-	public HandleClient(Socket  client) throws Exception {
-         // get input and output streams
-	 input = new BufferedReader( new InputStreamReader( client.getInputStream())) ;
-	 output = new PrintWriter ( client.getOutputStream(),true);
-	 // read name
-	 name  = input.readLine();
-	 users.add(name); // add to vector
-	 start();
-        }
+		public HandleClient(Socket  client) throws Exception {
+			// get input and output streams
+			input = new BufferedReader( new InputStreamReader( client.getInputStream())) ;
+			output = new PrintWriter ( client.getOutputStream(),true);
+			// read name
+			name  = input.readLine();
+			users.add(name); // add to vector
+			start();
+		}
 
-        public void sendMessage(String uname,String  msg)  {
-	    output.println( uname + ":" + msg);
-	}
+        public void sendMessage(String uname,String  msg) {
+	  		output.println( uname + ":" + msg);
+		}
 		
         public String getUserName() {  
             return name; 
         }
         public void run()  {
-    	     String line;
-	     try    {
-                while(true)   {
-		 line = input.readLine();
-		 if ( line.equals("end") ) {
-		   clients.remove(this);
-		   users.remove(name);
-		   break;
-                 }
-		 broadcast(name,line); // method  of outer class - send messages to all
-	       } // end of while
-	     } // try
-	     catch(Exception ex) {
-	       System.out.println(ex.getMessage());
-	     }
+			String line;
+			 
+			try{
+				while(true){
+					line = input.readLine();
+					if (line.equals("end")){
+						clients.remove(this);
+						users.remove(name);
+						break;
+					}
+					broadcast(name,line); // method  of outer class - send messages to all
+				} // end of while
+			} // try
+			catch(Exception ex) {
+			System.out.println(ex.getMessage());
+			}
         } // end of run()
-   } // end of inner class
+   	} // end of inner class
 
 } // end of Server
